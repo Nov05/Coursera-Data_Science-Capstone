73a5f06fe90627ab2fe333bc986b701ebd8c6fe8
Nov05 <arwen.liuwenjing@gmail.com>
2018-12-11 03:31:54 -0600

    Add 02.SampleText

--- 01.CodeExamples.R
+++ 01.CodeExamples.R
@@ -4,8 +4,11 @@
 ## https://www.coursera.org/learn/data-science-project/supplement/IbTUL/task-1-getting-and-cleaning-the-data
 
 ## Load the data in
-dataset:con <- file("D:/R/data/capstone/en_US/en_US.twitter.txt", "r") 
+path <- "D:/R/data/capstone/en_US/en_US.twitter.txt"
+con <- file(path, "r") 
 readLines(con, 1) ## Read the first line of text 
 readLines(con, 1) ## Read the next line of text 
 readLines(con, 5) ## Read in the next 5 lines of text 
 close(con) ## It's important to close the connection when you are done
+
+
